138a1894dbe4a40eff9083e9ddee3d1d31ef7364
alphadude <alphadude4all@gmail.com>
2026-08-03 14:34:45 +0100

    chore: convert bamboo logo to webp format and update sponsor data reference

--- lib/data/sponsors.ts
+++ lib/data/sponsors.ts
@@ -65,7 +65,7 @@ export const sponsors: Sponsor[] = [
   {
     id: "bamboo",
     name: "Bamboo",
-    logoUrl: "/logo/bamboo.tiff",
+    logoUrl: "/logo/bamboo.webp",
     websiteUrl: "https://investbamboo.com",
     tier: "gold",
   },
